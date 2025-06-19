0d8c5f777b30ee19262b80e88223b48540b4e652
ChrisM709 <christopher.meadus@keyin.com>
2025-06-19 16:27:41 -0300

    Small Change to unused import

--- Airport-API/src/main/java/com/airportAPI/rest/aircraft/AircraftService.java
+++ Airport-API/src/main/java/com/airportAPI/rest/aircraft/AircraftService.java
@@ -2,7 +2,6 @@ package com.airportAPI.rest.aircraft;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import jakarta.persistence.EntityNotFoundException;
 
