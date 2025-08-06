780fe1421d1ffb7fb8b3dc22a00e301cd7dc1fd9
Noah Devine <Noah.Devine@keyin.com>
2025-08-06 13:36:04 -0230

    Added cross orgin to airport controller

--- Airport-API/src/main/java/com/airportAPI/rest/airport/AirportController.java
+++ Airport-API/src/main/java/com/airportAPI/rest/airport/AirportController.java
@@ -4,6 +4,7 @@ import com.airportAPI.rest.aircraft.Aircraft;
 import com.airportAPI.rest.gates.Gates;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class AirportController {
     @Autowired
     private AirportService airportService;
